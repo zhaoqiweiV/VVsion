fe9c089f2fbf53893f1c4ea7335626a4623623d5
= <zhaoqiwei@zhaoqiweideimac.local>
2017-02-27 14:56:36 +0800

    Initial Commit

---
+++ VVsion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VVsion
+//
+//  Created by 赵祺伟 on 17/2/27.
+//  Copyright © 2017年 KM赵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VVsion/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VVsion
+//
+//  Created by 赵祺伟 on 17/2/27.
+//  Copyright © 2017年 KM赵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
